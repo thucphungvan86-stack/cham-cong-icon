9b1516f62b170a9357d4a0ac798aa621f04c01ed
thucphungvan86-stack <thucphungvan86@gmail.com>
2026-09-26 14:54:25 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'cham-cong-launcher-v2-4-5';
+const CACHE_NAME = 'cham-cong-launcher-v2-4-6-iconfix-1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.webmanifest',
-  './CHAM_CONG_ICON_180.png',
-  './CHAM_CONG_ICON_192.png',
-  './CHAM_CONG_ICON_512.png'
+  './manifest.webmanifest?v=2.4.6',
+  './CHAM_CONG_ICON_V2_4_6_180.png',
+  './CHAM_CONG_ICON_V2_4_6_192.png',
+  './CHAM_CONG_ICON_V2_4_6_512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -28,7 +28,7 @@ self.addEventListener('fetch', event => {
   if (url.origin !== self.location.origin) return;
 
   event.respondWith(
-    fetch(event.request)
+    fetch(event.request, { cache: 'no-store' })
       .then(response => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
